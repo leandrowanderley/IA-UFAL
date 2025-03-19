1ec51d4a6c4d0627923154fd447ab685d644b851
leandrowanderley <devleandromcz@gmail.com>
2025-03-19 00:02:01 -0300

    Add decision tree implementations for C4.5, CART, and ID3 algorithms

---
+++ c4_5.py
@@ -0,0 +1,57 @@
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+from sklearn.tree import DecisionTreeClassifier, export_text
+import matplotlib.pyplot as plt
+from sklearn import tree
+
+data = {
+    'Histórico de Crédito': [
+        'Ruim', 'Desconhecido', 'Desconhecido', 'Desconhecido', 'Desconhecido',
+        'Desconhecido', 'Ruim', 'Ruim', 'Boa', 'Boa', 'Boa', 'Boa', 'Boa',
+        'Ruim', 'Desconhecido', 'Boa', 'Ruim', 'Boa', 'Desconhecido', 'Boa'
+    ],
+    'Dívida': [
+        'Alta', 'Alta', 'Baixa', 'Baixa', 'Baixa', 'Baixa', 'Baixa', 'Baixa', 
+        'Baixa', 'Alta', 'Alta', 'Alta', 'Alta', 'Alta', 'Alta', 'Baixa', 
+        'Baixa', 'Baixa', 'Alta', 'Alta'
+    ],
+    'Garantia': [
+        'Nenhuma', 'Nenhuma', 'Nenhuma', 'Nenhuma', 'Nenhuma', 'Adequada', 
+        'Nenhuma', 'Adequada', 'Nenhuma', 'Adequada', 'Nenhuma', 'Nenhuma', 
+        'Nenhuma', 'Nenhuma', 'Adequada', 'Nenhuma', 'Adequada', 'Adequada', 
+        'Nenhuma', 'Adequada'
+    ],
+    'Renda': [
+        '$0 a $15k', '$15k a $35k', '$15k a $35k', '$0 a $15k', 'Acima de $35k',
+        'Acima de $35k', '$0 a $15k', 'Acima de $35k', 'Acima de $35k', 
+        'Acima de $35k', '$0 a $15k', '$15k a $35k', 'Acima de $35k', '$15k a $35k',
+        '$15k a $35k', '$15k a $35k', '$15k a $35k', '$0 a $15k', 'Acima de $35k',
+        '$15k a $35k'
+    ],
+    'Risco': [
+        'Alto', 'Alto', 'Moderado', 'Alto', 'Baixo', 'Baixo', 'Alto', 'Moderado',
+        'Baixo', 'Baixo', 'Alto', 'Moderado', 'Baixo', 'Alto', 'Moderado', 'Baixo',
+        'Moderado', 'Baixo', 'Baixo', 'Moderado'
+    ]
+}
+
+df = pd.DataFrame(data)
+
+le = LabelEncoder()
+
+for column in df.columns:
+    if df[column].dtype == 'object':
+        df[column] = le.fit_transform(df[column])
+
+x = df.drop('Risco', axis=1)
+y = df['Risco']
+
+clf_c45 = DecisionTreeClassifier(criterion='entropy', max_depth=3)
+clf_c45.fit(x, y)
+
+tree_rules_c45 = export_text(clf_c45, feature_names=x.columns.tolist())
+print("Árvore de Decisão (Regras - C4.5):\n", tree_rules_c45)
+
+plt.figure(figsize=(12,8))
+tree.plot_tree(clf_c45, filled=True, feature_names=x.columns, class_names=le.classes_)
+plt.show()
