bfb060dfa38ee53554d696da83a3d9eb97f83384
leandrowanderley <devleandromcz@gmail.com>
2025-04-22 16:17:57 -0300

    Sentiment Analyzer

---
+++ sentiment-analyzer/app.py
@@ -0,0 +1,15 @@
+# Construção da aplicação de Análise de Sentimento em Python com Streamlit e Hugging Face
+# pip install transformers streamlit
+# streamlit run /Users/leandrowanderley/Documents/programacao/IA-UFAL/sentiment-analyzer/app.py
+
+from transformers import pipeline
+import streamlit as st
+
+st.title("Análise de Sentimento")
+
+text = st.text_area("Digite um texto:")
+
+if st.button("Analisar"):
+    classifier = pipeline("sentiment-analysis")
+    result = classifier(text)[0]
+    st.write(f"**Sentimento:** {result['label']} (score: {result['score']:.2f})")
